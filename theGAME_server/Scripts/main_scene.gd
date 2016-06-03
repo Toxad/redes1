31ee59cec78fcfb941703bd0dd7dbda269da828a
Novandrie <gabriel.5489@gmail.com>
2016-06-03 14:27:22 -0300

    dicionario com user/ip

--- theGAME_server/Scripts/main_scene.gd
+++ theGAME_server/Scripts/main_scene.gd
@@ -3,6 +3,10 @@ extends Panel
 var global_obj
 
 var peers_numb = 0
+var connected_users = []														#armazenamento de usuarios
+var new_user = {}
+var username
+var useraddr
 
 func _ready():
 	self.get_node("ListPanel").add_style_override("panel", get_stylebox("bg", "Tree"))
@@ -11,15 +15,18 @@ func _ready():
 
 func _process(delta):
 	if(global_obj.listening()):
-		var recev = global_obj.receive_packet()
+		var recev = global_obj.receive_packet()									#recev[i] onde i: 0=autenticação; 1=nome; 2=mensagem; 3=endereço 
 		if(recev != null):
 			print("Received something")
 			if(recev[0] == 10):
-				global_obj.send(15,recev[3])									#"ack" para cliente
+				username = recev[1]
+				useraddr = recev[3]
+				global_obj.send(15,useraddr)									#"ack" para cliente
 				peers_numb = peers_numb + 1
 				self.get_node("PeersNumber").set_text(str(peers_numb))			#atualiza numero de usuarios
-				self.get_node("ListPanel/List").add_text(recev[1])				#adiciona nome de usuário à lista
-				self.get_node("ListPanel/List").newline()
+				self.get_node("ListPanel/List").add_text(username+"--"+useraddr+"\n")				#adiciona usuário ao display
+				new_user.useraddr = username									#criação de novo usuário conectado
+				connected_users.append(new_user)								#adiciona à lista interna
 		else:
 			return
 	else:
