72e26f02eba7dd6ae4809f01c414f27b38984b4b
Novandrie <gabriel.5489@gmail.com>
2016-06-05 15:08:56 -0300

    correção em classes

--- theGAME_client/Scripts/Skills/Mage/fireball.gd
+++ theGAME_client/Scripts/Skills/Mage/fireball.gd
@@ -9,7 +9,7 @@ func _init():
 	self.set_icon("res://Images/Fireball.tex")
 	
 func call(player):
-	var dmg = 1.2*(player.get_magi_atk())
+	var dmg = 1.2*(player.get_magic_atk())
 	self.set_damage(dmg)
 	self.set_mana_cost(mana_cost)
 
--- theGAME_client/Scripts/Skills/Necromancer/unfathomable_darkness.gd
+++ theGAME_client/Scripts/Skills/Necromancer/unfathomable_darkness.gd
@@ -9,7 +9,7 @@ func _init():
 	self.set_icon("res://Images/Unfathomable_Darkness.tex")
 	
 func call(player):
-	var dmg = 1.5*(player.get_magi_atk())
+	var dmg = 1.5*(player.get_magic_atk())
 	self.set_damage(dmg)
 	self.set_mana_cost(mana_cost)
 
