fe12eda3ea8e5e9e12c4c61fb3df0cf3315948f4
Mon <mon@crayworld.com>
2016-06-05 00:30:42 -0300

    uhsauhsau

--- theGAME_client/Scripts/player.gd
+++ theGAME_client/Scripts/player.gd
@@ -22,7 +22,18 @@ func _init(name, attributes, skills, job):
 	self.attributes_dict["str"] = attributes[0]
 	self.attributes_dict["agi"] = attributes[1]
 	self.attributes_dict["int"] = attributes[2]
-	self.attributes_dict["luk"] = attributes[3]
+	#self.attributes_dict["luk"] = 
+	var val = 0
+	for i in range(attributes[3]):
+		if(val == 0):
+			self.set_strength(self.get_strength() + 1)
+			val = val + 1
+		elif(val == 1):
+			self.set_agility(self.get_agility() + 1)
+			val = val + 1
+		elif(val == 2):
+			self.set_intelligence(self.get_intelligence() + 1)
+			val = 0
 	self.skills_arr = skills
 	self.magic_def = randi() % 10
 	self.phys_def = 10 - self.magic_def
@@ -90,6 +101,57 @@ func get_luck():
 
 func set_luck():
 	self.attributes_dict["luk"] = val
+
+func get_phys_attack():
+	return self.get_strength() * 3
+
+func get_magic_attack():
+	return self.get_intelligence() * 4
+	pass
+
+func get_critical_melee():
+	var total = 0
+	var val = self.get_strength()
+	while(val >= 3):
+		total = total + 5
+		val = val - 3
+	val = self.get_agility()
+	while(val >= 6):
+		total = total + 5
+		val = val - 6
+	return total
+
+func get_critical_ranged():
+	var total = 0
+	var val = self.get_agility()
+	while(val >= 3):
+		total = total + 5
+		val = val - 3
+	val = self.get_strength()
+	while(val >= 6):
+		total = total + 5
+		val = val - 6
+	return total
+
+func get_critical_magic():
+	var total = 0
+	var val = self.get_intelligence()
+	while(val >= 4):
+		total = total + 5
+		val = val - 4
+	return total
+
+func get_dodge():
+	var total = 0
+	var val = self.get_agility()
+	while(val >= 2):
+		total = total + 5
+		val = val - 2
+	val = self.get_intelligence()
+	while(val >= 3):
+		total = total + 2
+		val = val - 3
+	return total
 
 func pass_turn_status():
 	var to_remove = []
