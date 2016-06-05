97b4b5334979b63310e9a3d02b8118727ef9c993
Novandrie <gabriel.5489@gmail.com>
2016-06-05 00:06:28 -0300

    da bom

--- theGAME_client/Scripts/create_char.gd
+++ theGAME_client/Scripts/create_char.gd
@@ -1,13 +1,19 @@
 
 extends Panel
 
+var currentPanel = null
+var job = null
 var global_obj
+var points = 15
+
+var Str = 0
+var Int = 0
+var Agi = 0
+var Luk = 0
 
 func _ready():
 	global_obj = self.get_parent().get_node("/root/globalNode")
 	pass
-
-
 
 
 func _on_BackButton_pressed():
@@ -16,3 +22,42 @@ func _on_BackButton_pressed():
 
 func _on_InfoButton_pressed():
 	global_obj.changeScene("res://Scenes/info_scene.scn")
+
+
+func _on_BerserkerButton_pressed():
+	job = "Berserker"
+	changePanel("AttributesPanel")
+
+func _on_HunterButton_pressed():
+	job = "Hunter"
+	changePanel("AttributesPanel")
+
+
+func _on_KnightButton_pressed():
+	job = "Knight"
+	changePanel("AttributesPanel")
+
+
+func _on_MageButton_pressed():
+	job = "Mage"
+	changePanel("AttributesPanel")
+
+func _on_NecromancerButton_pressed():
+	job = "Necromancer"
+	changePanel("AttributesPanel")
+
+func _on_RogueButton_pressed():
+	job = "Rogue"
+	changePanel("AttributesPanel")
+
+func _on_AttBackButton_pressed():
+	job = null
+	changePanel("ClassPanel")
+
+
+
+func changePanel(msg):
+	if(currentPanel != null):
+		currentPanel.hide()
+	currentPanel = self.get_node(msg)
+	currentPanel.show()
