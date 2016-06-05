1120037151dd3dc13c597167358a3f7859fd549a
Novandrie <gabriel.5489@gmail.com>
2016-06-05 20:05:14 -0300

    tooltip

--- theGAME_client/Scripts/Skills/Berserker/frenzy.gd
+++ theGAME_client/Scripts/Skills/Berserker/frenzy.gd
@@ -5,7 +5,7 @@ func _init():
 	self.set_name("Frenzy")
 	self.set_mana_cost(50)
 	self.set_icon("res://Images/Frenzy.tex")
-
+	self.set_info("Gain 4 STR points for 4 turns at the cost of 20 HP per turn.")
 
 func call(player):
 	frenzy = ("res://Scripts/Status/frenzy_buff.gd")
--- theGAME_client/Scripts/create_char.gd
+++ theGAME_client/Scripts/create_char.gd
@@ -235,10 +235,8 @@ func _on_SkillsBackButton_pressed():
 	self.reset_skill_buttons()
 	changePanel("AttributesPanel")
 
-func _on_Skill1_mouse_enter():
-	pass # replace with function body
 
-	
+####BOTAO 1####
 func _on_Skill1_toggled(pressed):
 	if(pressed):																	#botão toggles, adiciona skill do botão correspondente no dicionario 
 		self.get_node("SkillsPanel/Control/Skill1/Sprite").set_self_opacity(0.5)
@@ -247,6 +245,14 @@ func _on_Skill1_toggled(pressed):
 		self.get_node("SkillsPanel/Control/Skill1/Sprite").set_self_opacity(1)		#retira do dicionario
 		selected_skills.erase(button_skills[0].get_name())
 		
+func _on_Skill1_mouse_enter():
+	get_node("SkillsPanel/Control/Skill1/Tooltip/SkillInfo1").set_text(button_skills[0].get_info())
+	get_node("SkillsPanel/Control/Skill1/Tooltip").show()
+
+func _on_Skill1_mouse_exit():
+	get_node("SkillsPanel/Control/Skill1/Tooltip").hide()
+
+####BOTAO 2####
 func _on_Skill2_toggled(pressed):
 	if(pressed):
 		self.get_node("SkillsPanel/Control/Skill2/Sprite").set_self_opacity(0.5)
@@ -254,7 +260,15 @@ func _on_Skill2_toggled(pressed):
 	else:
 		self.get_node("SkillsPanel/Control/Skill2/Sprite").set_self_opacity(1)
 		selected_skills.erase(button_skills[1].get_name())
-		
+
+func _on_Skill2_mouse_enter():
+	get_node("SkillsPanel/Control/Skill2/Tooltip/SkillInfo2").set_text(button_skills[1].get_info())
+	get_node("SkillsPanel/Control/Skill2/Tooltip").show()
+
+func _on_Skill2_mouse_exit():
+	get_node("SkillsPanel/Control/Skill2/Tooltip").hide()
+
+####BOTAO 3####
 func _on_Skill3_toggled(pressed):
 	if(pressed):
 		self.get_node("SkillsPanel/Control/Skill3/Sprite").set_self_opacity(0.5)
@@ -262,7 +276,8 @@ func _on_Skill3_toggled(pressed):
 	else:
 		self.get_node("SkillsPanel/Control/Skill3/Sprite").set_self_opacity(1)
 		selected_skills.erase(button_skills[2].get_name())
-		
+
+####BOTAO 4####
 func _on_Skill4_toggled(pressed):
 	if(pressed):
 		self.get_node("SkillsPanel/Control/Skill4/Sprite").set_self_opacity(0.5)
@@ -336,3 +351,4 @@ func _on_CancelButton_pressed():
 	
 
 	
+
