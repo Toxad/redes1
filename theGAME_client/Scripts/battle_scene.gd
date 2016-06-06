2fa4ca6821041f5cb1f7bcd5f96e886948b042d5
Mon <mon@crayworld.com>
2016-06-05 21:21:32 -0300

    42842

--- theGAME_client/Scripts/battle_scene.gd
+++ theGAME_client/Scripts/battle_scene.gd
@@ -7,9 +7,14 @@ func _ready():
 	self.get_node("DialoguePanel/VBoxContainer/AttackButton").grab_focus()
 	self.get_node("DialoguePanel/VBoxContainer").set_pos(Vector2(680,2))
 	self.get_node("DialoguePanel/VBoxContainer").set_alignment(1)
-
 	self.set_process(true)
+	self.set_process_input(true)
+
 func _process(delta):
+	pass
+
+func _input(event):
+	
 	pass
 
 #FOCO NO BOTÃO#
@@ -63,3 +68,26 @@ func _on_FleeButton_mouse_enter():
 	if(not self.get_node("DialoguePanel/Pointer").is_visible()):
 		self.get_node("DialoguePanel/Pointer").show()
 	self.get_node("DialoguePanel/Pointer").set_pos(Vector2(670,164))
+
+# PRESSED BOTÃO
+
+func _on_AttackButton_pressed():
+	pass # replace with function body
+
+
+func _on_SkillsButton_pressed():
+	pass # replace with function body
+
+
+func _on_DefendButton_pressed():
+	pass # replace with function body
+
+
+func _on_ItemsButton_pressed():
+	pass # replace with function body
+
+
+func _on_FleeButton_pressed():
+	pass # replace with function body
+
+
